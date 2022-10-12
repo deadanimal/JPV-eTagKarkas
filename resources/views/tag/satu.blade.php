549de197a8cf46050b6d91c6b144d237db7c5c6c
Mohamad Zachroy Anazfitry <zachroy.anazfitry@pipeline-network.com>
2022-10-12 17:45:50 +0800

    Views Tag & Daging

--- resources/views/tag/satu.blade.php
+++ resources/views/tag/satu.blade.php
@@ -1,1 +1,107 @@
-satu...
+<div>
+    <div>
+        <div class="row mb-3">
+            <div class="col">
+                <nav style="--falcon-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%23748194'/%3E%3C/svg%3E&#34;);"
+                    aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="#" class="text-secondary">Permohonan Tag Karkas</a>
+                        </li>
+                        <li class="breadcrumb-item text-dark-green-jkr" style="font-weight: 700" aria-current="page">
+                            Daftar Tag Karkas
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        
+        <hr class="text-primary mb-3">
+        
+        
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header bg-oranje-jkr">
+                    <b>Permohonan Tag Karkas</b>
+                </div>
+                <div class="card-body">
+                        
+                   
+
+                    <!--Nama Pengeluar-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Nama Pengeluar:</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="nama" value="{{$daftar->}}" readonly />
+                    </div>
+                    </div>
+                    <!--Induk-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Induk:</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="induk" value="{{$daftar}}" readonly  />
+                    </div>
+                    </div>
+                    <!--Pejabat Pengeluar-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Pejabat Pengeluar:</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="pejabat" value="{{$daftar}}" readonly    />
+                    </div>
+                    </div>
+                    <!--Kategori Tag-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Kategori Tag</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="tag" value="{{$daftar}}" readonly    />
+                    </div>
+                    </div>
+                    <!--Jenis Ternakan-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Jenis Ternakan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="tag" value="{{$daftar}}" readonly />
+                    </div>
+                    </div>
+                    {{-- Bilangan Ternakan --}}
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Bilangan Ternakan</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" autocapitalize="off" name="bil_ternakan" value="{{$tag}}" readonly  />
+                        </div>
+                    </div>
+                    {{-- Bilangan Kod Bar Untuk Dijana --}}
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Bilangan Kod Bar Untuk Dijana</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" autocapitalize="off" name="bil_kod" placeholder="16" value="{{$tag}}" readonly  />
+                        </div>
+                    </div>
+                     {{-- Pengesyoran Kuantiti --}}
+                     <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Pengesyoran Kuantiti</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" autocapitalize="off" name="bil_kod" placeholder="16" value="{{$tag}}" readonly  />
+                        </div>
+                    </div>
+
+                    <!--Cetakan Kod Bar-->
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Cetakan Kod Bar</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" autocapitalize="off" name="cetakan" value="{{$daftar}}" readonly   />
+                    </div>
+                    </div>         
+                            
+                    <!--Button-->
+                    <div align="right">
+                        <button class=“btn btn-primary” type=“submit”>Simpan/Tolak</button>
+                        <button class=“btn btn-primary” type=“submit”>Hantar/Lulus</button>
+                    </div>
+        
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/tag/senarai.blade.php
+++ resources/views/tag/senarai.blade.php
@@ -1,5 +1,76 @@
-senarai
+<div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+            <div class="card p-3  py-4">
+                <div class="row g-3 mt-2">
+                    <div class="col-md-3">
+                        <label for="nama_projek">Nombor Dokumen</label>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" id="myInput" class="form-control" placeholder="xxxxx">
+                    </div>
+                    <div class="col-md-3">
+                        <button class="btn btn-light btn-block">Carian</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-@foreach ($tags as $tag)
-{{$tag}}
+    <div class="row d-flex justify-content-center">
+        <table class="table table-bordered">
+            <thead class="text-white ">
+                <tr>
+                    <th scope="col">Bil.</th>
+                    <th scope="col">Nombor Dokumen</th>
+                    <th scope="col">Pejabat Pengeluar</th>
+                    <th scope="col">Jenis Ternakan</th>
+                    <th scope="col">Bilangan Ternakan</th>
+                    <th scope="col">Kategori Tag</th>
+                    <th scope="col">Tarikh Rekod</th>
+                    <th scope="col">Pemohon Oleh</th>
+                    <th scope="col">Cetakan Kod Bar</th>
+                    <th scope="col">Status</th>
+                    
+                </tr>
+            </thead> 
+            <tbody id="myTable">
+               
+                <tr>
+                    @foreach ($tags as $tag)
+
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    <td>{{ $tag }}</td>
+                    
+                    {{-- Button edit & hapus --}}
+                    <td>
+                        <div class="col-auto">
+                            <a href="#"
+                                class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></a>
+                        </div>
+                        <div class="col-auto">
+                            <form action="#" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-outline-primary"><i class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </div>
+                    </td>
+ 
+                </tr>
+                    @endforeach
+      
+            </tbody>
+        </table>
+    </div>
+    
+
+
 @endforeach
