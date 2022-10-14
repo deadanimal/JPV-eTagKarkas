1d1e037c07fe28b183a06f97edac8f2a3d27cc23
Afeez Aziz <afeezaziz@gmail.com>
2022-10-14 09:53:58 +0800

    321

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,20 @@ Route::middleware(['auth'])->group(function () {
     Route::get('rumah', [RumahSembelihController::class, 'senarai_rumah']);
     Route::post('rumah', [RumahSembelihController::class, 'cipta_rumah']);
     
-    Route::get('daging', [DagingController::class, 'senarai_daging']);
-    Route::get('daging/{id}', [DagingController::class, 'satu_daging']);
+    Route::get('daging', [DagingController::class, 'senarai_daging']);    
     Route::post('daging', [DagingController::class, 'cipta_daging']);
+    Route::get('daging/{id}', [DagingController::class, 'satu_daging']);
     Route::post('daging/{id}/sebelum-sembelih', [DagingController::class, 'cipta_sebelum_sembelih']);
     Route::post('daging/{id}/selepas-sembelih', [DagingController::class, 'cipta_selepas_sembelih']);
     Route::post('daging/{id}/laporan', [DagingController::class, 'cipta_laporan']);
     
     Route::get('tag', [TagController::class, 'senarai_tag']);
     Route::get('tag/{id}', [TagController::class, 'satu_tag']);
-    Route::post('tag', [TagController::class, 'cipta_tag']);    
+    Route::post('tag', [TagController::class, 'cipta_tag']);   
+    
+    Route::get('tag', [TagController::class, 'senarai_tag']);
+    Route::get('tag/{id}', [TagController::class, 'satu_tag']);
+    Route::post('tag', [TagController::class, 'cipta_tag']);      
 
 });
 
