fded90f699bc0c1695a53a49ad6d236cce6751bf
Afeez Aziz <afeezaziz@gmail.com>
2022-11-02 10:03:14 +0800

    jhkj

--- resources/views/daging/ruminan.blade.php
+++ resources/views/daging/ruminan.blade.php
@@ -16,211 +16,184 @@
         </ul>
 
         <div class="tab-content">
-            <div class="header">
-                <h1 class="header-title text-black">
-                    Ruminan
-                </h1>
-            </div>
+
             <div class="tab-pane active show" id="tab-1" role="tabpanel">
 
-                <form action="">
-                    <div class="card-header">
-                        <b>Pemeriksaan Daging > Pengenalan Ternakan</b>
+
+            
+                    <div class="row">
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Nama Pemilik</label>
+                            <input type="text" placeholder="Zachroy Anazfitry" name="pemilik">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">No Kenderaan</label>
+                            <input type="text" placeholder="ABC 123" name="kenderaan">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Masa Ternakan Tiba Dipremis</label>
+                            <input type="time" name="tiba">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Masa Ternakan Disembelih</label>
+                            <input type="time" name="disembelih">
+                        </div>
+
                     </div>
-                    <div class="card-body">
-                        <div class="row">
 
-                            <div class="mb-3 col-md-3">
-                                <label for="">Nama Pemilik</label>
-                                <input type="text" placeholder="Zachroy Anazfitry" name="pemilik">
-                            </div>
+                    <div class="row">
 
-                            <div class="mb-3 col-md-3">
-                                <label for="">No Kenderaan</label>
-                                <input type="text" placeholder="ABC 123" name="kenderaan">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Masa Ternakan Tiba Dipremis</label>
-                                <input type="time" name="tiba">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Masa Ternakan Disembelih</label>
-                                <input type="time" name="disembelih">
-                            </div>
-
+                        <div class="mb-3 col-md-3">
+                            <label for="">Nombor Permit</label>
+                            <input type="number" placeholder="12345" name="permit">
                         </div>
 
-                        <div class="row">
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Nombor Permit</label>
-                                <input type="number" placeholder="12345" name="permit">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Spesis Ternakan</label>
-                                <input type="text" placeholder="Lembu" name="spesis">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Bilangan Ternakan (Mengikut SKV)</label>
-                                <input type="number" placeholder="12" name="bil_ternakan">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">ID/No Permis</label>
-                                <input type="number" name="id_permis">
-                            </div>
-
+                        <div class="mb-3 col-md-3">
+                            <label for="">Spesis Ternakan</label>
+                            <input type="text" placeholder="Lembu" name="spesis">
                         </div>
 
-                        <div class="row">
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Nama Premis</label>
-                                <input type="text" name="nama_premis">
-                            </div>
-
-                            <div class="mb-3 col-md-3">
-                                <label for="">Alamat Premis</label>
-                                <textarea name="alamat" id="" cols="20" rows="3"></textarea>
-                            </div>
-
+                        <div class="mb-3 col-md-3">
+                            <label for="">Bilangan Ternakan (Mengikut SKV)</label>
+                            <input type="number" placeholder="12" name="bil_ternakan">
                         </div>
 
-                        <!--Button-->
-                        <div>
-                            <button class="btn btn-primary" type="submit"
-                                style="margin-left: auto;margin-right: 0;">Kemaskini</button>
-                            <button class="btn btn-primary" type="submit"
-                                style="margin-left: auto;margin-right: 0;">Simpan</button>
+                        <div class="mb-3 col-md-3">
+                            <label for="">ID/No Permis</label>
+                            <input type="number" name="id_permis">
                         </div>
+
                     </div>
-                </form>
+
+                    <div class="row">
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Nama Premis</label>
+                            <input type="text" name="nama_premis">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Alamat Premis</label>
+                            <textarea name="alamat" id="" cols="20" rows="3"></textarea>
+                        </div>
+
+                    </div>
+
+                    <!--Button-->
+                    <div>
+                        <button class="btn btn-primary" type="submit"
+                            style="margin-left: auto;margin-right: 0;">Kemaskini</button>
+                    </div>
+                
+
             </div>
 
             <div class="tab-pane" id="tab-2" role="tabpanel">
 
-                <form action="">
+                <div class="card-body">
 
-                    <div class="card">
-                        <div class="card-header">
-                            <b>Pemeriksaan Daging > Maklumat Haiwan Ternakan</b>
+                    <div class="row">
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Bilangan Ternakan Yang Diterima</label>
+                            <input type="number" name="bil_ternakan_diterima" placeholder="2">
                         </div>
 
-                        <div class="card-body">
-
-                            <div class="row">
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Bilangan Ternakan Yang Diterima</label>
-                                    <input type="number" name="bil_ternakan_diterima" placeholder="2">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Ternakan Yang Mati Semasa Tiba</label>
-                                    <input type="number" name="ternakan_mati_semasa_tiba" placeholder="2">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Jumlah Ternakan Yang Diperiksa</label>
-                                    <input type="number" name="jumlah_ternakan_diperiksa" placeholder="2">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Jumlah Binatang Layak Disembelih</label>
-                                    <input type="number" name="jumlah_binatang_layak_disembelih" placeholder="2">
-                                </div>
-
-                            </div>
-
-                            <div class="row">
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Bilangan Diasingkan Untuk Pemeriksaan Rapi</label>
-                                    <input type="text" name="bilangan_diasingkan_pemeriksaan_rapi">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Jumlah Binatang Disembelih Kerana Kecemasan</label>
-                                    <input type="number" name="jumlah_binatang_disembelih_kecemasan" placeholder="2">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Jumlah Yang Disembelih</label>
-                                    <input type="number" name="jumlah_disembelih" placeholder="2">
-                                </div>
-
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Baki Ternakan Yang Belum Disembelih</label>
-                                    <input type="number" name="baki_ternakan_belum_disembelih" placeholder="2">
-                                </div>
-
-                            </div>
-
-                            <div class="row">
-                                <div class="mb-3 col-md-3">
-                                    <label for="">Catatan</label>
-                                    <textarea name="catatan" cols="60" rows="5"></textarea>
-                                </div>
-                            </div>
+                        <div class="mb-3 col-md-3">
+                            <label for="">Ternakan Yang Mati Semasa Tiba</label>
+                            <input type="number" name="ternakan_mati_semasa_tiba" placeholder="2">
                         </div>
 
-                        <hr>
-
-                        <div class="container">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Bil</th>
-                                        <th scope="col">No Tag</th>
-                                        <th scope="col">Jantina</th>
-                                        <th scope="col">Baka</th>
-                                        <th scope="col">Tarikh Disembelih</th>
-                                        <th scope="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>XXXX</td>
-                                        <td>Betina</td>
-                                        <td>XXXX</td>
-                                        <td>20/7/2022</td>
-                                        <td>
-                                            <button class="far fa-edit"></button>
-                                            <button class="fas fa-trash"></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>XXXX</td>
-                                        <td>Jantan</td>
-                                        <td>XXXX</td>
-                                        <td>20/7/2022</td>
-                                        <td>
-                                            <button class="far fa-edit"></button>
-                                            <button class="fas fa-trash"></button>
-                                        </td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
+                        <div class="mb-3 col-md-3">
+                            <label for="">Jumlah Ternakan Yang Diperiksa</label>
+                            <input type="number" name="jumlah_ternakan_diperiksa" placeholder="2">
                         </div>
 
-                        <!--Button-->
-                        <div>
-                            <button class="btn btn-primary" type="submit"
-                                style="margin-left: auto;margin-right: 0;">Kemaskini</button>
-                            <button class="btn btn-primary" type="submit"
-                                style="margin-left: auto;margin-right: 0;">Simpan</button>
+                        <div class="mb-3 col-md-3">
+                            <label for="">Jumlah Binatang Layak Disembelih</label>
+                            <input type="number" name="jumlah_binatang_layak_disembelih" placeholder="2">
                         </div>
 
                     </div>
 
-                </form>
+                    <div class="row">
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Bilangan Diasingkan Untuk Pemeriksaan Rapi</label>
+                            <input type="text" name="bilangan_diasingkan_pemeriksaan_rapi">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Jumlah Binatang Disembelih Kerana Kecemasan</label>
+                            <input type="number" name="jumlah_binatang_disembelih_kecemasan" placeholder="2">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Jumlah Yang Disembelih</label>
+                            <input type="number" name="jumlah_disembelih" placeholder="2">
+                        </div>
+
+                        <div class="mb-3 col-md-3">
+                            <label for="">Baki Ternakan Yang Belum Disembelih</label>
+                            <input type="number" name="baki_ternakan_belum_disembelih" placeholder="2">
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="mb-3 col-md-3">
+                            <label for="">Catatan</label>
+                            <textarea name="catatan" cols="60" rows="5"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="container">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Bil</th>
+                                <th scope="col">No Tag</th>
+                                <th scope="col">Jantina</th>
+                                <th scope="col">Baka</th>
+                                <th scope="col">Tarikh Disembelih</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>XXXX</td>
+                                <td>Betina</td>
+                                <td>XXXX</td>
+                                <td>20/7/2022</td>
+                                <td>
+                                    <button class="far fa-edit"></button>
+                                    <button class="fas fa-trash"></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>XXXX</td>
+                                <td>Jantan</td>
+                                <td>XXXX</td>
+                                <td>20/7/2022</td>
+                                <td>
+                                    <button class="far fa-edit"></button>
+                                    <button class="fas fa-trash"></button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
 
             <div class="tab-pane" id="tab-3" role="tabpanel">
