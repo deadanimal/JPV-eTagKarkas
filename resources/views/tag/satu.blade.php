2abb126be56385833ece23a8d07740cb054bae8c
Mohamad Zachroy Anazfitry <zachroy.anazfitry@pipeline-network.com>
2022-11-03 14:49:33 +0800

    modal

--- resources/views/tag/satu.blade.php
+++ resources/views/tag/satu.blade.php
@@ -302,9 +302,32 @@
 
                 @role('pentadbir')
                 @if ($tag->status == 'Hantar' || $tag->status == 'Simpan')
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-3">
-                        <button class="btn btn-success" type="submit" name="submitbutton" value="Sah">Sahkan</button>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-success " data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Sahkan
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                            Adakah anda pasti untuk mengesahkan permohonan?
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                            <button class="btn btn-success " type="submit" value="submit">Ya</button>
+                        </div>
+                        </div>
+                        </div>
                     </div>
+
+                    {{-- <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-3">
+                        <button class="btn btn-success" type="submit" name="submitbutton" value="Sah">Sahkan</button>
+                    </div> --}}
                 @endif 
                 @if ($tag->status == 'Sah' )
                 <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-3">
