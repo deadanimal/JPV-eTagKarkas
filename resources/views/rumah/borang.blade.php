3070850119470f92613123f8ee2ba39978d081c8
Afeez Aziz <afeezaziz@gmail.com>
2022-11-08 09:22:32 +0800

    dsa

--- resources/views/rumah/borang.blade.php
+++ resources/views/rumah/borang.blade.php
@@ -34,16 +34,6 @@
                     <b class="text-center">Pendaftaran Premis</b>
                 </div>
 
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
                 @if (Session::has('success'))
                     <div class="alert alert-success">
                         {{ Session::get('success') }}
